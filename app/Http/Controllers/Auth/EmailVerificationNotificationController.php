f468331e72bdea6d8080e7925b4a7afee5aa89ef
Nuuriii <nuriii.dev@gmail.com>
2025-02-12 06:55:34 +0700

    fix: remove unused code

--- app/Http/Controllers/Auth/EmailVerificationNotificationController.php
+++ app/Http/Controllers/Auth/EmailVerificationNotificationController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 class EmailVerificationNotificationController extends Controller
 {
     /**
-     * Send a new emai
+     * Send a 
      */
     public function store(Request $request): RedirectResponse
     {
