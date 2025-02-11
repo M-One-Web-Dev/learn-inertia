f551c38b166c28ae866115b8b954c5a7f560f5a4
Nuuriii <nuriii.dev@gmail.com>
2025-02-11 23:39:35 +0700

    fix: remove unused code

--- resources/js/Pages/dashboard/programming-languange/page.tsx
+++ resources/js/Pages/dashboard/programming-languange/page.tsx
@@ -6,7 +6,7 @@ export default function Dashboard() {
         <AuthenticatedLayout
             breadcrumb={[{ name: 'Languange', url: '/dashboard/languange' }]}
         >
-            <Head title="Dashboard - Language" />
+            <Head title="Language" />
 
             <div className="py-12">
                 <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
