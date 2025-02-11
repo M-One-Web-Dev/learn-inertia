715ed6c84ff4333fe83b8281c5af1e7c2287d419
Nuuriii <nuriii.dev@gmail.com>
2025-02-12 06:55:52 +0700

    fix: remove unused code

--- app/Http/Controllers/Auth/EmailVerificationNotificationController.php
+++ app/Http/Controllers/Auth/EmailVerificationNotificationController.php
@@ -8,9 +8,7 @@ use Illuminate\Http\Request;
 
 class EmailVerificationNotificationController extends Controller
 {
-    /**
-     
-     */
+    
     public function store(Request $request): RedirectResponse
     {
         if ($request->user()->hasVerifiedEmail()) {
