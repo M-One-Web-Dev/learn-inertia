afb1533ae927467b3993c53e117031d2c4dd9185
Nuuriii <nuriii.dev@gmail.com>
2025-02-12 06:55:11 +0700

    fix: remove unused code

--- app/Http/Controllers/Auth/EmailVerificationPromptController.php
+++ app/Http/Controllers/Auth/EmailVerificationPromptController.php
@@ -10,7 +10,6 @@ use Inertia\Response;
 
 class EmailVerificationPromptController extends Controller
 {
-   
     public function __invoke(Request $request): RedirectResponse|Response
     {
         return $request->user()->hasVerifiedEmail()
